aa19d94e46d5ee7c34c37d2f54f28cf2fd0f3f79
lthoedt <lthoedt@gmail.com>
2020-12-19 14:24:47 +0100

    titles added

--- build/top2000/src/pages/Playing.jsx
+++ build/top2000/src/pages/Playing.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { BlockTitle, Block } from 'framework7-react';
+
 import ReactAudioPlayer from 'react-audio-player';
 import ReactPlayer from 'react-player'
 
@@ -19,9 +21,9 @@ export default function Playing() {
 	if (playing.refreshed === false) {
 		setTimeout(() => {
 			dispatch(playingSongGet());
-			dispatch({type: "PLAYING_REFRESHED_SET", refreshed: false})
+			dispatch({ type: "PLAYING_REFRESHED_SET", refreshed: false })
 		}, 10000);
-		dispatch({type: "PLAYING_REFRESHED_SET", refreshed: true})
+		dispatch({ type: "PLAYING_REFRESHED_SET", refreshed: true })
 	}
 
 	return (
@@ -29,8 +31,9 @@ export default function Playing() {
 			<div className="page-content" style={{ padding: 0 }}>
 				<div style={{ height: '100%', width: '100%', backgroundImage: `url(${playing.song.image})`, backgroundPosition: 'center', position: 'fixed', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', filter: 'blur(50px)', zIndex: '-9' }}><div style={{ width: '100%', height: '100%', backgroundColor: 'rgba(0, 0, 0, 0.6)' }}></div></div>
 
+				<BlockTitle>Wat speelt er nu?</BlockTitle>
 				<Cover src={playing.song.image} />
-				<div style = {{textAlign: 'center'}}>
+				<div style={{ textAlign: 'center' }}>
 					<ReactAudioPlayer
 						src="https://icecast.omroep.nl/radio2-bb-mp3"
 						// autoPlay
@@ -39,20 +42,24 @@ export default function Playing() {
 					/>
 				</div>
 
-				<div className = "centerContent" style={{flexDirection: 'column'}}>
-					<span className = "title">{playing.song.title}</span>
-					<span className = "subTitle">{playing.song.artist}</span>
+				<div className="centerContent" style={{ flexDirection: 'column' }}>
+					<span className="title">{playing.song.title}</span>
+					<span className="subTitle">{playing.song.artist}</span>
 				</div>
 
-				<div className="centerContent">
-					<ReactPlayer
-						url='https://nl-ams-p5-am5.cdn.streamgate.nl/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MDg0NjUyMjQsInVyaSI6IlwvbGl2ZVwvbnBvXC91XC9ucG9cL25vZHJtXC9kYXNoX3VuZW5jcnlwdGVkXC9ucG8tdnNyLTJcLzBcLzBcLzBcL25wby12c3ItMi5pc21sIiwidmlld2VyIjoiYzVlYjM0OTlhNWU5MDNkYWNjNjIzNGZjNjFkM2RjYzIzMjIwZGZiMCIsInJpZCI6IjYxYWQyMTcifQ.KVI1wXAYMlTpH0Of5UVYmN7Qp9SWGnHWrWXTbw4CUV0/live/npo/u/npo/nodrm/dash_unencrypted/npo-vsr-2/0/0/0/npo-vsr-2.isml/stream.mpd' 
-						controls
-						playing={false}
-					/>
-				</div>
+				<BlockTitle>Live studio</BlockTitle>
 
-			</div>
+				<Block>
+					<div className="centerContent">
+						<ReactPlayer
+							url='https://nl-ams-p5-am5.cdn.streamgate.nl/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MDg0NjUyMjQsInVyaSI6IlwvbGl2ZVwvbnBvXC91XC9ucG9cL25vZHJtXC9kYXNoX3VuZW5jcnlwdGVkXC9ucG8tdnNyLTJcLzBcLzBcLzBcL25wby12c3ItMi5pc21sIiwidmlld2VyIjoiYzVlYjM0OTlhNWU5MDNkYWNjNjIzNGZjNjFkM2RjYzIzMjIwZGZiMCIsInJpZCI6IjYxYWQyMTcifQ.KVI1wXAYMlTpH0Of5UVYmN7Qp9SWGnHWrWXTbw4CUV0/live/npo/u/npo/nodrm/dash_unencrypted/npo-vsr-2/0/0/0/npo-vsr-2.isml/stream.mpd'
+							controls
+							height="100%"
+							playing={false}
+						/>
+					</div>
+				</Block>
+					</div>
 		</div>
 	);
 }
