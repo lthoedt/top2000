d81b7444dc03c247c3d19209c53139452526ca6c
lthoedt <38183200+lthoedt@users.noreply.github.com>
2020-12-24 22:22:41 +0100

    Delete config.js

--- build/top2000API/database/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	// mongoURI: 'mongodb://localhost:27017/top2000',
-	mongoURI: 'mongodb+srv://top2000db:top2000db!1@cluster0.5iywd.mongodb.net/top2000db?retryWrites=true&w=majority',
-	// mongoURI: 'mongodb://top2000db:top2000db!1@cluster0-shard-00-00.5iywd.mongodb.net:27017,cluster0-shard-00-01.5iywd.mongodb.net:27017,cluster0-shard-00-02.5iywd.mongodb.net:27017/top2000db?ssl=true&replicaSet=atlas-buoaoh-shard-0&authSource=admin&retryWrites=true&w=majority',
-	testMongoURI: 'mongodb://localhost:27017/testTop2000'
-};
